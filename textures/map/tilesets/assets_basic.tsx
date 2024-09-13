adae11753dea5a95895629af815710fbdcb89c5f
ajolan <jolan.allen@ynov.com>
2024-09-13 13:56:44 +0200

    iiifu

--- textures/map/tilesets/assets_basic.tsx
+++ textures/map/tilesets/assets_basic.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="assets_basic" tilewidth="40" tileheight="40" tilecount="9" columns="3">
+<tileset version="1.10" tiledversion="1.11.0" name="assets_basic" tilewidth="20" tileheight="20" tilecount="36" columns="6">
  <image source="simple_tileset_32x32_version1.0/assets_basic.png" width="128" height="128"/>
 </tileset>
