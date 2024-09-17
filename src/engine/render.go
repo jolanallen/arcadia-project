f89cec756bd0024436ab9f1487c385da62e0fdc4
ajolan <jolan.allen@ynov.com>
2024-09-17 16:05:56 +0200

    jj

--- src/engine/render.go
+++ src/engine/render.go
@@ -56,7 +56,7 @@ func (e *Engine) InGameRendering() {
 	rl.DrawTextEx(vrm, "Inventory", rl.Vector2{X: 1500, Y: 1000}, 40, 2, rl.Black) // rajouter le tableau en faut faire une boucle le tableau est dans init.go
 	rl.DrawTextEx(vrm, "Inventory", rl.Vector2{X: 1500, Y: 1000}, 40, 2, rl.Black) // rajouter le tableau en faut faire une boucle le tableau est dans init.go
 	rl.DrawText("Press [P] to Pause", int32(rl.GetScreenWidth())/2-rl.MeasureText("Press [P] to Pause", 20)/2, int32(rl.GetScreenHeight())/2-490, 20, rl.RayWhite)
-	rl.DrawText(fmt.Sprint("FPS:", int32(rl.GetFPS())), 1600, 50, 50, rl.Black)
+	rl.DrawText(fmt.Sprint("fps:", int32(rl.GetFPS())/2-rl.MeasureText("fps:", 20)), 1600, 50, 20, rl.Black)
 
 	rl.DrawTexturePro(
 		e.SpriteLife,
